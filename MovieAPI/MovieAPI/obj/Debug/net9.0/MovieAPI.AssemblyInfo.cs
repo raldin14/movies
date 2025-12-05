d883ab1cfa8ec2044853810bdf31f3f14d59f066
raldin14 <raldin.hidalgo@gmail.com>
2025-12-05 12:55:03 -0600

    Removing conexion strings

--- MovieAPI/MovieAPI/obj/Debug/net9.0/MovieAPI.AssemblyInfo.cs
+++ MovieAPI/MovieAPI/obj/Debug/net9.0/MovieAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b6f4167649a3da4301e9f8e598e4397c30966cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
